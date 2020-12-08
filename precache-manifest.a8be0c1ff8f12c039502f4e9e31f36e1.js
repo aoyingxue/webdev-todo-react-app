9d631f83cb3926e8e93c56435211655c1c2c16c6
Yuki Ao <aoyingxue@gmail.com>
2020-12-08 10:55:25 -0600

    Updates

---
+++ precache-manifest.a8be0c1ff8f12c039502f4e9e31f36e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0557929672c85132d21",
+    "url": "/09-todo-react-aoyingxue/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "f0557929672c85132d21",
+    "url": "/09-todo-react-aoyingxue/static/js/main.f0557929.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/09-todo-react-aoyingxue/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "b802277d12b269adaec2",
+    "url": "/09-todo-react-aoyingxue/static/js/runtime~main.b802277d.js"
+  },
+  {
+    "revision": "9cb0c73e57c55b51988274d71f0db260",
+    "url": "/09-todo-react-aoyingxue/index.html"
+  }
+];
