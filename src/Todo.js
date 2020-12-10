a870fb79e11d6b25646b8c6d9bbdfecd75a51094
Yuki Ao <aoyingxue@gmail.com>
2020-12-10 10:08:15 -0600

    change username

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Todo.css';
+
+class Todo extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ToDo App</h1>
+      </div>
+    );
+  }
+}
+
+export default Todo;
